cae86d73321956d2fa276cd3ff561ba10914b39b
Sourav Rout <sourav.9911rout@gmail.com>
2025-11-12 06:40:40 +0000

    the credit fonts are not matching change it with others

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ export default {
         body: ['Inter', 'sans-serif'],
         headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
-        credit: ['Times New Roman', 'serif'],
+        credit: ['Inter', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
